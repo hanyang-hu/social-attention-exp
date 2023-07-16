73b20f314f4e20a9a2ec9f2e8ab8b204c325cc4e
Hanyang Hu <63213964+KoHomerHu@users.noreply.github.com>
2023-07-17 00:28:26 +0800

    Add moving_average method

--- rl_utils.py
+++ rl_utils.py
@@ -22,6 +22,14 @@ def filter_non_presense(state):
     # return state[state[:, 0] != 0][:, 1:]
     return state
 
+def moving_average(a, window_size):
+    cumulative_sum = np.cumsum(np.insert(a, 0, 0)) 
+    middle = (cumulative_sum[window_size:] - cumulative_sum[:-window_size]) / window_size
+    r = np.arange(1, window_size-1, 2)
+    begin = np.cumsum(a[:window_size-1])[::2] / r
+    end = (np.cumsum(a[:-window_size:-1])[::2] / r)[::-1]
+    return np.concatenate((begin, middle, end))
+
 def train_off_policy_agent(env, agent, num_episodes, replay_buffer, minimal_size, batch_size):
     return_list = []
     for i in range(100):
@@ -36,7 +44,6 @@ def train_off_policy_agent(env, agent, num_episodes, replay_buffer, minimal_size
                 while not (done or truncated):
                     action = agent.take_action(state)
                     next_state, reward, done, truncated, _ = env.step(action)
-                    done = done or truncated
                     next_state = filter_non_presense(next_state)
                     replay_buffer.add(state, action, reward, next_state, done)
                     state = next_state
