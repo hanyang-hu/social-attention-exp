0fccfc3a58ad527f195e30f53503d71199c8cd4c
Hanyang Hu <63213964+KoHomerHu@users.noreply.github.com>
2023-07-17 23:08:18 +0800

    Allow rgb_rendering

--- rl_utils.py
+++ rl_utils.py
@@ -32,7 +32,7 @@ def moving_average(a, window_size):
 
 def train_off_policy_agent(env, agent, num_episodes, replay_buffer, minimal_size, batch_size):
     return_list = []
-    for i in range(50):
+    for i in range(300):
         with tqdm(total=int(num_episodes/10), desc='Iteration %d' % i) as pbar:
             for i_episode in range(int(num_episodes/10)):
                 episode_return = 0
@@ -44,6 +44,7 @@ def train_off_policy_agent(env, agent, num_episodes, replay_buffer, minimal_size
                 while not (done or truncated):
                     action = agent.take_action(state)
                     next_state, reward, done, truncated, _ = env.step(action)
+                    reward = 10 * reward
                     next_state = filter_non_presense(next_state)
                     replay_buffer.add(state, action, reward, next_state, done)
                     state = next_state
@@ -52,6 +53,36 @@ def train_off_policy_agent(env, agent, num_episodes, replay_buffer, minimal_size
                         b_s, b_a, b_r, b_ns, b_d = replay_buffer.sample(batch_size)
                         transition_dict = {'states': b_s, 'actions': b_a, 'next_states': b_ns, 'rewards': b_r, 'dones': b_d}
                         agent.update(transition_dict)
+                return_list.append(episode_return)
+                if (i_episode+1) % 10 == 0:
+                    pbar.set_postfix({'episode': '%d' % (num_episodes/10 * i + i_episode+1), 'return': '%.3f' % np.mean(return_list[-10:])})
+                pbar.update(1)
+    return return_list
+
+def train_off_policy_agent_with_rendering(env, agent, num_episodes, replay_buffer, minimal_size, batch_size):
+    return_list = []
+    for i in range(300):
+        with tqdm(total=int(num_episodes/10), desc='Iteration %d' % i) as pbar:
+            for i_episode in range(int(num_episodes/10)):
+                episode_return = 0
+                state, _ = env.reset()
+                # print("Previous state: {}".format(state))
+                state = filter_non_presense(state)
+                # print("Filtered state: {}".format(state))
+                done = truncated = False
+                while not (done or truncated):
+                    action = agent.take_action(state)
+                    next_state, reward, done, truncated, _ = env.step(action)
+                    reward = 10 * reward
+                    next_state = filter_non_presense(next_state)
+                    replay_buffer.add(state, action, reward, next_state, done)
+                    state = next_state
+                    episode_return += reward
+                    if replay_buffer.size() > minimal_size:
+                        b_s, b_a, b_r, b_ns, b_d = replay_buffer.sample(batch_size)
+                        transition_dict = {'states': b_s, 'actions': b_a, 'next_states': b_ns, 'rewards': b_r, 'dones': b_d}
+                        agent.update(transition_dict)
+                    image = env.render()
                 return_list.append(episode_return)
                 if (i_episode+1) % 10 == 0:
                     pbar.set_postfix({'episode': '%d' % (num_episodes/10 * i + i_episode+1), 'return': '%.3f' % np.mean(return_list[-10:])})
